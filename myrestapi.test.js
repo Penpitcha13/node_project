badb5da1d4fc622f1abf951ccb509e12620820bb
Penpitcha <penpitchatong13@gmail.com>
2025-04-02 11:57:04 +0700

    add 2 more test cases

--- myrestapi.test.js
+++ myrestapi.test.js
@@ -4,4 +4,10 @@ describe('test hr zone calculation', () => {
   test('test - zone number is correct?', () => {
     expect(restapi.calculateHR(30,180).zone).toBe(5);
   });
+  test('test - lb hr is correct?', () => {
+    expect(restapi.calculateHR(30,180).zone).toBe(5);
+  });
+  test('test - up hr is correct?', () => {
+    expect(restapi.calculateHR(30,180).zone).toBe(5);
+  });
 });
